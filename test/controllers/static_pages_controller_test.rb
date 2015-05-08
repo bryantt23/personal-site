9264e7b71eb03f545dc3b1ab3a6260a7b5c47428
bryantt23 <bryantt23@gmail.com>
2015-05-08 03:43:32 -0700

    removed future chapter gems, passed title tests, refactored display of titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | Bryant Tunbutr's Webpage"
   end
 
   test "should get resources" do
     get :resources
     assert_response :success
+    assert_select "title", "Resources | Bryant Tunbutr's Webpage"
   end
 
 end
