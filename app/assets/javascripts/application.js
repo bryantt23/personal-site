f8dfd2e403618c81c30bf4646294ad9b9f34e1e4
bryantt23 <bryantt23@gmail.com>
2015-08-08 01:15:51 -0700

    using html5 slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,11 +17,12 @@
 //= require owl.carousel
 //= require bootstrap-sprockets
 //= require jQueryRotate
+//= require rangeslider
 //= require turbolinks
 
 // <script>
 $(document).ready(function() {
-// owl carousel ********************************************************
+  // owl carousel ********************************************************
   $("#jumb2").owlCarousel({
 
     items : 13, //10 items above 1000px browser width
@@ -357,33 +358,28 @@ $(document).ready(function() {
 
 
 
-    // for tip calculator ********************************************************
-
+  // for tip calculator ********************************************************
+  // http://stackoverflow.com/questions/15480516/jquery-tip-calculator-formulas-with-variables-based-on-textarea-inputs
 
   $('button').click(function(){
-  var Total = $('.Total').val();
-  var Tip = $('.Tip').val();
-  // var NumberOfPeople = $('.NumberOfPeople').val();
-  var calculatedTip = ((Tip/100)*Total).toFixed(2);
-  var NumberOfPeople = 1;
-  var AdjTotal = (((Tip/100)*Total)+Total/1).toFixed(2);
-  var Result = (AdjTotal/NumberOfPeople).toFixed(2);
-  if(isNaN(AdjTotal) || Total<0 || Tip<0) {
+    var Total = $('.Total').val();
+    var Tip = $('.Tip').val();
+    // var NumberOfPeople = $('.NumberOfPeople').val();
+    var calculatedTip = ((Tip/100)*Total).toFixed(2);
+    var NumberOfPeople = 1;
+    var AdjTotal = (((Tip/100)*Total)+Total/1).toFixed(2);
+    var Result = (AdjTotal/NumberOfPeople).toFixed(2);
+    if(isNaN(AdjTotal) || Total<0 || Tip<0) {
       $('.result').remove();
       $('.error').remove();
       $('.price').append('<p class="error">Please enter valid numbers into the above fields.</p>');
-  }
-  else {
+    }
+    else {
       $('.error').remove();
       $('.result').remove();
       $('.price').append('<p class="result">Your tip is $' + calculatedTip + ' and your total is $' + AdjTotal + ' '  + '.</p>');
-  }
+    }
   });
-
-
-
-
-
 
 
 });
