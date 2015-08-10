eb610fc14b7b0d18030649a9edecc53b494ed0c2
bryantt23 <bryantt23@gmail.com>
2015-08-10 02:22:44 -0700

    fix typo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807044829) do
+ActiveRecord::Schema.define(version: 20150810083023) do
 
   create_table "ajax_calculations", force: :cascade do |t|
     t.string   "index"
@@ -84,6 +84,13 @@ ActiveRecord::Schema.define(version: 20150807044829) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "name"
+    t.boolean  "complete"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
