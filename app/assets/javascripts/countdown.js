83fe797b5a12035c74d32d370f7106bcfa977f95
bryantt23 <bryantt23@gmail.com>
2015-08-10 22:36:59 -0700

    start/reset, stop/resume functionality is working. works by hiding and showing buttons based on actions

--- app/assets/javascripts/countdown.js
+++ app/assets/javascripts/countdown.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
-var currentTime;
-var timer2;
-var restartTime;
+  var currentTime;
+  var timer2;
+  var restartTime;
 
   $("#start_timer").on("click", function(){
 
@@ -11,17 +11,20 @@ var restartTime;
     if($(this).text() == "Start"){  // check button label
       $(this).html("Reset");
 
-          jQuery(function ($) {
-            var twentyFiveMinutes = 60 * 25,
-            // var fiveMinutes = 60 * 5,
-            display = $('#time');
-            startTimer(twentyFiveMinutes, display);
-            // startTimer(fiveMinutes, display);
-          });
+      jQuery(function ($) {
+        var twentyFiveMinutes = 60 * 25,
+        // var fiveMinutes = 60 * 5,
+        display = $('#time');
+        startTimer(3, display);
+        // startTimer(twentyFiveMinutes, display);
+        // startTimer(fiveMinutes, display);
+      });
     }
     else{
       $(this).html("<span class='ui-button-text'>Start</span>");
       $("#time").html("25:00")
+      $("#resume_timer").hide();
+      $("#stop_timer").show();
     }
 
 
@@ -35,45 +38,65 @@ var restartTime;
     // console.log(currentTime);
     restartTime=timer2;
     clearInterval(currentTime);
-          console.log(timer2);
+    console.log(timer2);
+    if($("#start_timer").text() == "Reset"){  // check button label
+      $("#stop_timer").hide();
+      $("#resume_timer").show();
+    }
+
   });
 
 
-    $("#resume_timer").on("click", function(){
+
+
+  $("#resume_timer").on("click", function(){
     clearInterval(currentTime);
 
-          jQuery(function ($) {
-          restartTime=timer2;
-            display = $('#time');
-            startTimer(restartTime, display);
-            // startTimer(fiveMinutes, display);
-          });
-
+    jQuery(function ($) {
+      restartTime=timer2;
+      display = $('#time');
+      startTimer(restartTime, display);
+      // startTimer(fiveMinutes, display);
     });
-
-
-
-
-
-
-    function resumeTimer(duration, display) {
-      var timer = duration, minutes, seconds;
-      currentTime = setInterval(function () {
-        timer2=timer;
-        minutes = parseInt(timer / 60, 10)
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.text(minutes + ":" + seconds);
-
-        // console.log(timer2);
-        if (--timer < 0) {
-          timer = duration;
-        }
-      }, 1000);
+    if($("#start_timer").text() == "Reset"){  // check button label
+      $("#resume_timer").hide();
+      $("#stop_timer").show();
     }
+
+
+  });
+
+
+
+
+
+
+
+  function resumeTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    currentTime = setInterval(function () {
+      timer2=timer;
+      minutes = parseInt(timer / 60, 10)
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.text(minutes + ":" + seconds);
+
+      // console.log(timer2);
+      if (--timer < 0) {
+        // timer = duration;
+        clearInterval(currentTime);
+        $("#time").html("25:00")
+        $("#start_timer").html("Start");
+
+
+
+
+      }
+    }, 1000);
+  }
 
 
 
@@ -93,7 +116,10 @@ var restartTime;
 
       // console.log(timer2);
       if (--timer < 0) {
-        timer = duration;
+        // timer = duration;
+        clearInterval(currentTime);
+        $("#time").html("25:00")
+        $("#start_timer").html("Start");
       }
     }, 1000);
   }
