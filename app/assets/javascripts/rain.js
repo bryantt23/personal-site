211c1bbd53a37e80531776751d9d68f75a5ad82f
bryantt23 <bryantt23@gmail.com>
2015-08-11 03:49:42 -0700

    rains dolllar signs only once and shakes

--- app/assets/javascripts/rain.js
+++ app/assets/javascripts/rain.js
@@ -13,18 +13,16 @@ $(document).ready(function() {
       $( "#cloud").effect( "shake", {times:1,direction:"right"} );
     }
     anim();
-  });
 
+    fallingSnow();
 
+    function fallingSnow() {
+      console.log("hi");
 
+      var $snowflakes = $(), qt = 20;
 
-  // Snow Falling
-  function fallingSnow() {
-    var $snowflakes = $(),
-    createSnowflakes = function () {
-      var qt = 20;
       for (var i = 0; i < qt; ++i) {
-        var $snowflake = $('<div class="snowflakes"></div>');
+        var $snowflake = $('<div class="snowflakes">$</div>');
         $snowflake.css({
           'left': (Math.random() * $('#site').width()) + 'px',
           'top': (- Math.random() * $('#site').height()) + 'px'
@@ -33,35 +31,77 @@ $(document).ready(function() {
         $snowflakes = $snowflakes.add($snowflake);
       }
       $('#snowZone').prepend($snowflakes);
-    },
 
-    runSnowStorm = function() {
-      $snowflakes.each(function() {
-
-        var singleAnimation = function($flake) {
-          $flake.animate({
-            top: "500px",
-            opacity : "0",
-          }, Math.random() + 5000, function(){
-            // this particular snow flake has finished, restart again
-            $flake.css({
-              'top': (- Math.random() * $('#site').height()) + 'px',
-              'opacity': 1
-            });
-            singleAnimation($flake);
-          });
-        };
-        singleAnimation($(this));
+      $snowflakes.animate({
+        top: "500px",
+        opacity : "0",
+      }, Math.random() + 5000, function(){
+        $(this).remove();
+        // run again when all 20 snowflakes hit the floor
+        if (--qt < 1) {
+          // fallingSnow();
+        }
       });
-    };
+    }
 
-    createSnowflakes();
-    runSnowStorm();
+
+
+
+  });
+
+
+
+  // comment every method
+  /*
+  // Snow Falling
+  function fallingSnow() {
+  var $snowflakes = $(),
+  createSnowflakes = function () {
+  var qt = 20;
+  for (var i = 0; i < qt; ++i) {
+  var $snowflake = $('<div class="snowflakes">$</div>');
+  $snowflake.css({
+  'left': (Math.random() * $('#site').width()) + 'px',
+  'top': (- Math.random() * $('#site').height()) + 'px'
+  });
+  // add this snowflake to the set of snowflakes
+  $snowflakes = $snowflakes.add($snowflake);
   }
-  fallingSnow();
+  // The .prepend() method inserts the specified content as
+  // the first child of each element in the jQuery collection
+  $('#snowZone').prepend($snowflakes);
+  },
+
+  runSnowStorm = function() {
+  $snowflakes.each(function() {
+
+  var singleAnimation = function($flake) {
+  $flake.animate({
+  top: "500px",
+  opacity : "0",
+  }, Math.random() + 5000, function(){
+  // this particular snow flake has finished, restart again
+  $flake.css({
+  'top': (- Math.random() * $('#site').height()) + 'px',
+  'opacity': 1
+  });
+  // singleAnimation($flake);
+  });
+  };
+  singleAnimation($(this));
+  });
+  };
+
+  createSnowflakes();
+  runSnowStorm();
+  }
+
+  // this is called first, the entire method
+  // fallingSnow();
+  */
 
 
-
+  // Snow Falling
 
 
 
