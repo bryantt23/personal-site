e40bd716d7f0685a74b7f72f6f4d9a393c730cd6
bryantt23 <bryantt23@gmail.com>
2015-08-15 01:48:54 -0700

    added weather now project description, comments for the code

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -2,24 +2,26 @@ $(document).ready(function() {
 
   (function() {
 
+    // listen for weatherButton click
     $('#weatherButton').click(function(){
-      var blah = $('#cname').val();
-      $.getJSON("http://api.openweathermap.org/data/2.5/weather?APPID=" + API_WEATHER_KEY +"&q="+blah
+
+      // take the value entered in the city_name textbox
+      var cityEntered = $('#city_name').val();
+
+      // http://openweathermap.org/current
+      // it has to be similar to http://api.openweathermap.org/data/2.5/find?q=London
+
+      $.getJSON("http://api.openweathermap.org/data/2.5/weather?APPID=" + API_WEATHER_KEY +"&q="+cityEntered
       +"&units=imperial ", getCurrentWeather);
     });
 
-
-    // -- Constantes -------------------------------------------------------------
-
-    var API_WORLDTIME_KEY = "d6a4075ceb419113c64885d9086d5";
-    var API_WORLDTIME = "https://api.worldweatheronline.com/free/v2/tz.ashx?format=json&key="+ API_WORLDTIME_KEY +"&q=";
     var API_WEATHER_KEY = "cbe3a5258849eb31e096fd9bf0763eb8";
-    var API_WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?APPID=" + API_WEATHER_KEY + "&";
-    var API_FORECAST_URL = "http://api.openweathermap.org/data/2.5/forecast?APPID=" + API_WEATHER_KEY + "&";
     var IMG_WEATHER = "http://openweathermap.org/img/w/";
 
     // -- Variables --------------------------------------------------------------
 
+    // these match the json info i.e. http://api.openweathermap.org/data/2.5/find?q=London has
+    // {"temp":288.46,"pressure":1011,"humidity":77,"temp_min":287.04,"temp_max":289.82},
     var cities = [];
     var cityWeather = {};
     cityWeather.zone;
@@ -47,61 +49,21 @@ $(document).ready(function() {
     var nombreNuevaCiudad  = $("[data-input='cityAdd']");
     var savedCities        = $("[data-saved-cities]");
 
-    // -- Funciones --------------------------------------------------------------
-
-/*
-    function onLoad() {
-      // Eventos
-      $(formAddNuevaCiudad).hide();
-      $(buttonAdd).on('click', addNewCity);
-      $(nombreNuevaCiudad).on('keypress', function(e) {
-        if(e.which == 13) {
-          addNewCity(e);
-        }
-      });
-
-      $(savedCities).on('click', loadSavedCities);
-
-      // Detecta la posición e inicia la aplicación
-      // if (navigator.geolocation) {
-      //   navigator.geolocation.getCurrentPosition(getCoords, errorFound);
-      // } else {
-      //   alert("Tu navegador no soporta GeoLocation");
-      // }
-    };
-
-    function errorFound(error) {
-      alert('Un error ocurrió: ' + error.code);
-      // El código de error puede ser:
-      // 0: Error desconocido
-      // 1: Permiso denegado
-      // 2: Posición no disponible
-      // 3: Timeout
-    };
-
-    // function getCoords(position) {
-    //   var lat = position.coords.latitude;
-    //   var lon = position.coords.longitude;
-    //   $.getJSON(API_WEATHER_URL + "lat=" + lat + "&lon=" + lon, getCurrentWeather);
-    // }
-*/
     function getCurrentWeather(data) {
 
+      // get info from json that was stored above
       cityWeather.zone        = data.name;
       cityWeather.icon        = IMG_WEATHER + data.weather[0].icon + ".png";
       cityWeather.temp        = data.main.temp;
       cityWeather.temp_max    = data.main.temp_max;
       cityWeather.temp_min    = data.main.temp_min;
-      // cityWeather.temp        = data.main.temp - 273.15;
-      // cityWeather.temp_max    = data.main.temp_max - 273.15;
-      // cityWeather.temp_min    = data.main.temp_min - 273.15;
       cityWeather.sunrise     = data.sys.sunrise;
       cityWeather.sunset      = data.sys.sunset;
       cityWeather.description = data.weather[0].description;
       cityWeather.main        = data.weather[0].main;
 
+      // this is how the info gets displayed
       renderTemplate(cityWeather, null);
-      // $("#weather-info").append(cityWeather);
     }
 
     function activateTemplate(id) {
@@ -120,8 +82,6 @@ $(document).ready(function() {
         timeToShow = timeNow;
       }
 
-      // Pinta los datos
-      // clone.querySelector("[data-time]").innerHTML            = timeToShow;
       clone.querySelector("[data-city]").innerHTML            = city.zone;
       clone.querySelector("[data-icon]").src                  = city.icon;
       clone.querySelector("[data-temp='max']").innerHTML      = "Max temp: " + city.temp_max.toFixed(1);
@@ -130,94 +90,17 @@ $(document).ready(function() {
 
       $(loader).hide();
       $(formAddNuevaCiudad).show();
-      // $($body).append(clone);
+
+      // append it to this id 
       $("#weather-info").append(clone);
 
-        $('#weatherButton').focus();
+      $('#weatherButton').focus();
 
-        // animate scrolling to bottom of the page
-        $("html, body").animate({ scrollTop: $(document).height() }, 500);
+      // animate scrolling to bottom of the page
+      // http://stackoverflow.com/questions/12299987/jquery-stay-at-bottom-of-page
+      $("html, body").animate({ scrollTop: $(document).height() }, 500);
 
-        // jump directly to the bottom of the page
-        //$("html, body").scrollTop($(document).height());
     }
-
-/*
-    function addNewCity(e) {
-      e.preventDefault();
-      $.getJSON(API_WEATHER_URL + "q=" + $(nombreNuevaCiudad).val(), getWeatherNewCity);
-    }
-
-    function getWeatherNewCity(data) {
-      if(data.cod && data.cod == '404') {
-        alert("Error: No existe esa ciudad en la base de datos")
-      }
-
-      $.getJSON(API_WORLDTIME + $(nombreNuevaCiudad).val(), function(response) {
-
-        $(nombreNuevaCiudad).val("");
-
-        //var newCity = {};
-        cityWeather = {};
-        cityWeather.zone        = data.name;
-        cityWeather.icon        = IMG_WEATHER + data.weather[0].icon + ".png";
-        cityWeather.temp        = data.main.temp - 273.15;
-        cityWeather.temp_max    = data.main.temp_max - 273.15;
-        cityWeather.temp_min    = data.main.temp_min - 273.15;
-        cityWeather.sunrise     = data.sys.sunrise;
-        cityWeather.sunset      = data.sys.sunset;
-        cityWeather.description = data.weather[0].description;
-        cityWeather.main        = data.weather[0].main;
-
-        renderTemplate(cityWeather, response.data.time_zone[0].localtime);
-
-        cities.push(cityWeather);
-        localStorage.setItem( 'cities', JSON.stringify(cities) );
-      });
-    }
-
-    function loadSavedCities(e) {
-      e.preventDefault();
-
-      function renderCities(cities) {
-        cities.forEach(function(city) {
-          var cityLoad = city;
-          renderTemplate(cityLoad);
-        });
-      };
-
-      function removeItems() {
-        var citiesToDelete = $(".card");
-        if(citiesToDelete.length > 1) {
-          for(var i=1; i<citiesToDelete.length; i++) {
-            citiesToDelete[i].remove();
-          }
-        }
-      };
-
-      removeItems();
-      var cities = JSON.parse( localStorage.getItem('cities') );
-      renderCities(cities);
-
-      // Detecta eventos de click en cada ciudad
-      $(".card").on("click", function(e) {
-        var i = $(".card").index(this);
-        var result = window.confirm("¿Deseas eliminar la ciudad salvada?");
-
-        // Elimina la ciudad de localStorage
-        if(result) {
-          var cards = $(".card");
-          cards[i].remove();
-        }
-        cities.splice(i, 1);
-        console.log(cities);
-        localStorage.setItem('cities', JSON.stringify(cities) );
-      });
-    }
-*/
-    // -- Inicia la aplicación ---------------------------------------------------
-
-    // onLoad();
 
   })();
 });
