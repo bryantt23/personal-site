5295f195b2f9971ca2e748aed9fe683e3079bc49
bryantt23 <bryantt23@gmail.com>
2015-08-16 20:51:11 -0700

    checks for anagrams and whether word is too long or short

--- app/assets/javascripts/scrambled_word.js
+++ app/assets/javascripts/scrambled_word.js
@@ -37,9 +37,9 @@ $(document).ready(function() {
     });
 
     function areTheyTheSameWord(guessEntered){
-      console.log('answer');
+      // console.log('answer');
       // console.log(window['answer'].text);
-      console.log(answer);
+      // console.log(answer);
       // http://stackoverflow.com/questions/5963182/how-to-remove-spaces-from-a-string-using-javascript
       if(answer===guessEntered.toLowerCase().replace(/\s/g, '')){
         // console.log('same');
@@ -48,15 +48,32 @@ $(document).ready(function() {
       }
       else {
         // console.log('not same');
-        $("#guessResult").text("Your guess is incorrect!");
+        $("#guessResult").text("Your guess is incorrect! " );
         // alert('not same');
+        if(guessEntered.length<answer.length){
+          $("#guessResult").append("You have too few letters.");
+        }
+        else if(guessEntered.length>answer.length){
+          $("#guessResult").append("You have too many letters.");
+        }
+        else{
+          // check to see if the guess and answer are anagrams
+          // http://codereview.stackexchange.com/questions/99247/determining-if-two-words-are-anagrams
+          if(guessEntered.split('').sort().join('')==answer.split('').sort().join('')){
+            // if they are anagrams, say you are close, just move the letters around
+            $("#guessResult").append("You have the correct letters. Keep rearranging the letters to get the correct answer.");
+          }
+          else{
+            // if they are not anagrams, say you have letters that are not in the answer
+            $("#guessResult").append("Your guess has letter(s) that are not in the answer.");
+          }
+
+
+        }
+
 
       }
     };
-
-    // def are_they_the_same_word?(guess, answer)
-    //   return guess.gsub(/\s+/, "").downcase==answer.downcase
-    // end
 
     function getWord(){
       myArray = ["control", "power", "uranium", "weapons", "human", "slaves", "dictatorship", "oppressor", "tycoon", "tyrant", "sovereign", "exterminate", "terminate", "vanquish", "raze", "demolish", "institutionalize", "annihilate", "decimate", "eradicate", "obliterate", "slaughter", "belize", "squash"];
