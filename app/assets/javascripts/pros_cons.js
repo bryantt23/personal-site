d0995d161a5562ae2618209479d86b1a68a1dc73
bryantt23 <bryantt23@gmail.com>
2015-08-18 21:51:53 -0700

    can clear everything added

--- app/assets/javascripts/pros_cons.js
+++ app/assets/javascripts/pros_cons.js
@@ -1,25 +1,36 @@
 var foodList = [];
 
 $(function () {
-    $("#button1").on('click', 'input[type=submit]', function () {
+  $("#button1").on('click', 'input[type=submit]', function () {
     var input = $('#addFood').val();
-      if(input==null || input == "") {
-        alert("Pro must be filled out");
-      }
-      else {
-        $('#foods').append('<div><a href="#" class="item">X</a> ' + $('#addFood').val() + '</div>');
-      }
-    });
-    $(document).on('click', '.item', function () {
-        $(this).parent().remove();
-    });
+    if(input==null || input == "") {
+      alert("Pro must be filled out");
+    }
+    else {
+      $('#foods').append('<div><a href="#" class="item">X</a> ' + $('#addFood').val() + '</div>');
+    }
+  });
+  $(document).on('click', '.item', function () {
+    $(this).parent().remove();
+  });
 
-    var foodList2 = [];
+  var foodList2 = [];
 
-    $("#button2").on('click', 'input[type=submit]', function () {
-        $('#foods2').append('<div><a href="#" class="item">X</a> ' + $('#addFood2').val() + '</div>');
-    });
-    $(document).on('click', '.item', function () {
-        $(this).parent().remove();
-    });
+  $("#button2").on('click', 'input[type=submit]', function () {
+    $('#foods2').append('<div><a href="#" class="item">X</a> ' + $('#addFood2').val() + '</div>');
+  });
+  $(document).on('click', '.item', function () {
+    $(this).parent().remove();
+  });
+
+  $("#clearListButton").on('click', function () {
+    $("#situation_text").val('');
+    $("#addFood2").val('');
+    $("#addFood").val('');
+    $('#foods').empty();
+    $('#foods2').empty();
+    
+  });
+
+
 });
