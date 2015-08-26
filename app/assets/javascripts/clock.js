c6646702cf20b895df545b387b880e2e023572a7
bryantt23 <bryantt23@gmail.com>
2015-08-26 04:29:25 -0700

    moved  rotating pics images and changed address of the images

--- app/assets/javascripts/clock.js
+++ app/assets/javascripts/clock.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
 
     $.getJSON("http://www.telize.com/geoip?callback=?",
     function(json) {
-      document.write("Geolocation information for IP address : ", json.ip);
-      document.write("Country : ", json.country);
-      document.write("Latitude : ", json.latitude);
-      document.write("Longitude : ", json.longitude);
-      console.log("timezone : ", json.timezone);
+      // document.write("Geolocation information for IP address : ", json.ip);
+      // document.write("Country : ", json.country);
+      // document.write("Latitude : ", json.latitude);
+      // document.write("Longitude : ", json.longitude);
+      // console.log("timezone : ", json.timezone);
       // city
       // offset
 
