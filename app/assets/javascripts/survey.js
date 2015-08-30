ec8f8728092c31ece3fe5d3ad495a4e23413a052
bryantt23 <bryantt23@gmail.com>
2015-08-29 20:38:18 -0700

    also calculates total dynamically

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -26,8 +26,56 @@ $(document).ready(function() {
   var names = ["Joanna","Lisa","Tricia","Kim" ]
 
   for(var i=0; i < names.length; i++){
-    $( "ul.friends" ).append('<li>' + names[i] + '</li>');
+    // $( "ul.friends" ).append('<li>' + names[i] + '</li>');
+    // $( "ul.friends" ).append('<li>' + names[i] + '</li>');
+
+    $( "ul.friends" ).append('<input type="checkbox" name="myCheckbox" />' +names[i] +' <br />');
+    // $( "ul.friends" ).append("<input type="checkbox" />" + names[i] + "<br />");
+
   }
+
+
+
+
+    // stress test code********************************************************
+    function displayVals() {
+      calcUsage();
+    }
+
+    // variable for  checkboxes
+    var $myCheckbox = $('input[name="myCheckbox"]');
+
+    function calcUsage() {
+      var total = 0;
+
+      // for each checkbox
+      $myCheckbox.each(function() {
+
+        // if this checkbox is checked
+        if (this.checked)
+
+        // parse the value and add it to the total
+        total += 1;
+      });
+
+      //display message to user based on total
+      //display in the div named usertotal
+      if(total>299){
+        $("#usertotal").text(total + ". At risk of illness.");
+      }
+      else if (total>149){
+        $("#usertotal").text(total +". Risk of illness is moderate.");
+      }
+      else{
+        $("#usertotal").text(total +". Only have a slight risk of illness.");
+      }
+    }
+
+    $("select").change(displayVals);
+    displayVals();
+
+    $myCheckbox.click(calcUsage);
+
 
 
 });
