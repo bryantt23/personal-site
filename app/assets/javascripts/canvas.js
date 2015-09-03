7314172bf95d8c61b36ffeb675d0038d204197cd
bryantt23 <bryantt23@gmail.com>
2015-09-02 23:43:17 -0700

    square moves from top left to right and top right to bottom right

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -1,19 +1,40 @@
 
-function draw(){
-  var ctx = document.getElementById("my_canvas").getContext("2d");
-  // ctx.fillStyle = color || hex || rgba() || gradient || pattern
+function initCanvas(){
+    var ctx = document.getElementById('my_canvas').getContext('2d');
+    var cW = ctx.canvas.width, cH = ctx.canvas.height;
+    // Constructor for rectangular assets
+    function rectObj(){
+        this.x = 0, this.y = 0, this.w = 0;
+        this.render = function(ctx, rx, ry, rw, rh, clr){
+            ctx.fillStyle = clr;
+            ctx.fillRect(rx, ry, rw, rh);
+        }
+    }
+    // Establish Assets outside of the animate function
+    // to avoid re-creating them each time the function runs
+    var rect1 = new rectObj();
+    var rect2 = new rectObj();
+    rect1.w = 50;
+    rect2.w = 50;
+    // rect2.y = 100;
+    rect2.y = 00;
+    rect2.h = 50;
+    function animate(){
+        ctx.clearRect(0, 0, cW, cH);
+        // Draw here
+        rect1.render(ctx, rect1.x, rect1.y, rect1.w, 50, "magenta");
+        rect2.render(ctx, cW-rect2.w, rect2.y, 50, rect2.h, "blue");
+        rect1.x++;
+        rect2.y++;
+    }
+    var animateInterval = setInterval(animate, 30);
 
-  console.log(ctx.canvas.id+" "+ctx.canvas.width+" "+ctx.canvas.height)
-
-  ctx.fillStyle = "magenta";
-
-  ctx.fillRect(20,20,100,100);
-  ctx.fillRect(200,200,50,50);
-
-  ctx.strokeRect(20,20,100,100);
-  ctx.strokeRect(200,200,50,50);
-
+    ctx.canvas.addEventListener('click', function(event) {
+        clearInterval(animateInterval);
+        console.log(animateInterval);
+    });
 
 }
-
-window.onload = draw;
+window.addEventListener('load', function(event) {
+    initCanvas();
+});
