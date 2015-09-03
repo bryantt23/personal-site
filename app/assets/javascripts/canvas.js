ead57cd7e62128036ef57b9eccd5c5cd35fa1975
bryantt23 <bryantt23@gmail.com>
2015-09-02 23:48:16 -0700

    added a third square that moves diagonally

--- app/assets/javascripts/canvas.js
+++ app/assets/javascripts/canvas.js
@@ -14,18 +14,28 @@ function initCanvas(){
     // to avoid re-creating them each time the function runs
     var rect1 = new rectObj();
     var rect2 = new rectObj();
+    var rect3 = new rectObj();
     rect1.w = 50;
     rect2.w = 50;
     // rect2.y = 100;
     rect2.y = 00;
     rect2.h = 50;
+
+    rect3.x = 140;
+    rect3.y = 0;
+    rect3.h = 50;
+    rect3.w = 50;
     function animate(){
         ctx.clearRect(0, 0, cW, cH);
         // Draw here
         rect1.render(ctx, rect1.x, rect1.y, rect1.w, 50, "magenta");
         rect2.render(ctx, cW-rect2.w, rect2.y, 50, rect2.h, "blue");
+        rect3.render(ctx, rect3.x, rect3.y, rect3.w, rect3.h, "green");
         rect1.x++;
         rect2.y++;
+        rect3.x++;
+        rect3.y++;
+
     }
     var animateInterval = setInterval(animate, 30);
 
