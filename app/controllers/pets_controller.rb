0f9014de6e5f61db8fb10110098dd231db4dc901
bryantt23 <bryantt23@gmail.com>
2015-09-04 05:55:57 -0700

    likeswork, redirect to index on save

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -28,55 +28,62 @@ class PetsController < ApplicationController
 
     respond_to do |format|
       if @pet.save
-        format.html { redirect_to @pet, notice: 'Pet was successfully created.' }
-        format.json { render :show, status: :created, location: @pet }
-      else
-        format.html { render :new }
-        format.json { render json: @pet.errors, status: :unprocessable_entity }
+        format.html { flash[:notice] =  'Pet was successfully created.'
+          redirect_to action: "index"  }
+          format.json { render :show, status: :created, location: @pet }
+          
+        else
+          format.html { render :new }
+          format.json { render json: @pet.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # PATCH/PUT /pets/1
-  # PATCH/PUT /pets/1.json
-  def update
-    respond_to do |format|
-      if @pet.update(pet_params)
-        format.html { redirect_to @pet, notice: 'Pet was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pet }
-      else
-        format.html { render :edit }
-        format.json { render json: @pet.errors, status: :unprocessable_entity }
+    # PATCH/PUT /pets/1
+    # PATCH/PUT /pets/1.json
+    def update
+      respond_to do |format|
+        if @pet.update(pet_params)
+          format.html { redirect_to @pet, notice: 'Pet was successfully updated.' }
+          format.json { render :show, status: :ok, location: @pet }
+        else
+          format.html { render :edit }
+          format.json { render json: @pet.errors, status: :unprocessable_entity }
+        end
       end
     end
-  end
 
-  # DELETE /pets/1
-  # DELETE /pets/1.json
-  def destroy
-    @pet.destroy
-    respond_to do |format|
-      format.html { redirect_to pets_url, notice: 'Pet was successfully deleted.' }
-      format.json { head :no_content }
+    # DELETE /pets/1
+    # DELETE /pets/1.json
+    def destroy
+      @pet.destroy
+      respond_to do |format|
+        format.html { redirect_to pets_url, notice: 'Pet was successfully deleted.' }
+        format.json { head :no_content }
+      end
+    end
+
+
+    def upvote
+      @pet = Pet.find(params[:id])
+      @pet.votes.create
+      # redirect_to(pets_path)
+      #redirect_to topics_path
+      respond_to do |format|
+        format.html { redirect_to pets_path }
+        format.js
+      end
+    end
+
+
+    private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pet
+      @pet = Pet.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pet_params
+      params.require(:pet).permit(:name, :pet_pic)
     end
   end
-
-
-  def upvote
-    @pet = Pet.find(params[:id])
-    @pet.votes.create
-    redirect_to(pets_path)
-  end
-
-
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_pet
-    @pet = Pet.find(params[:id])
-  end
-
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def pet_params
-    params.require(:pet).permit(:name, :pet_pic)
-  end
-end
