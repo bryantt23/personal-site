7c58cb04325621f14c51ca82310405a6cec8eb03
bryantt23 <bryantt23@gmail.com>
2015-09-07 01:22:44 -0700

    added project description for bottles of beer

--- public/javascripts/bottles.js
+++ public/javascripts/bottles.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
-  // $(".slider").on("change", function(){console.log(this.value)});
-
+  bottlesOfBeer(0);
 
   $(".slider").on("change", function(){
     document.getElementById("range").innerHTML=this.value;
@@ -9,27 +8,26 @@ $(document).ready(function() {
     bottlesOfBeer(numOfBeers);
   });
 
-
-  // $(".slider").on("change", function(){
-  //   (this.value)=document.getElementById("range").innerHTML});
-
-  // function showValue(newValue)
-  // {
-  //   $(".slider").on("change", function(){console.log(this.value)});
-  //   $(".slider").on("change", function(){(this.value)=newValue});
-  //   document.getElementById("range").innerHTML=newValue;
-  //   bottlesOfBeer(newValue);
-  // }
-
   function bottlesOfBeer(numOfBeers){
     var lyrics="";
     // console.log("yolo");
-    while(numOfBeers>0){
-      lyrics += numOfBeers + " bottles of beer on the wall, " + numOfBeers + " bottles of beer.<br>" + 
+    while(numOfBeers>2){
+      lyrics += numOfBeers + " bottles of beer on the wall, " + numOfBeers + " bottles of beer.<br>" +
       "Take one down, pass it around, " + --numOfBeers + " bottles of beer on the wall...<br>";
     }
-    lyrics += "No more bottles of beer on the wall, no more bottles of beer.<br>" +
-    "Go to the store and buy some more, 99 bottles of beer on the wall...";
+    if(numOfBeers==2){
+      lyrics += numOfBeers + " bottles of beer on the wall, " + numOfBeers + " bottles of beer.<br>" +
+      "Take one down, pass it around, " + --numOfBeers + " bottle of beer on the wall...<br>";
+    }
+    if(numOfBeers==1){
+      lyrics += numOfBeers + " bottle of beer on the wall, " + numOfBeers + " bottle of beer.<br>" +
+      "Take one down, pass it around, " + --numOfBeers + " bottles of beer on the wall...<br>";
+    }
+    if(numOfBeers==0) {
+        lyrics += "No more bottles of beer on the wall, no more bottles of beer.<br>" +
+        "Go to the store and buy some more, 99 bottles of beer on the wall...";
+    }
+
 
 
 
