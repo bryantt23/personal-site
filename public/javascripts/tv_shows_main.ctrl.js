3e7bc322775b3654472abfc69bd96b55e70d828c
bryantt23 <bryantt23@gmail.com>
2015-09-07 17:52:28 -0700

    added filter

--- public/javascripts/tv_shows_main.ctrl.js
+++ public/javascripts/tv_shows_main.ctrl.js
@@ -1,5 +1,33 @@
 
-  angular.module('app').controller("MainController", function(){
-      var vm = this;
-      vm.title = 'AngularJS Tutorial Example';
-    });
+angular.module('app').controller("MainController", function(){
+  var vm = this;
+  vm.title = 'AngularJS Tutorial Example';
+  vm.searchInput = '';
+    vm.shows = [
+        {
+            title: 'Game of Thrones',
+            year: 2011,
+            favorite: false
+        },
+        {
+            title: '24',
+            year: 2001,
+            favorite: true
+        },
+        {
+            title: 'Legend of Korra',
+            year: 2012,
+            favorite: true
+        },
+        {
+            title: 'Teen Titans Go!',
+            year: 2013,
+            favorite: true
+        },
+        {
+            title: 'Greys Anatomy',
+            year: 2005,
+            favorite: false
+        }
+    ];
+});
