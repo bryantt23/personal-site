ccf991e330373ade2ee750ff93ae217997985a2d
bryantt23 <bryantt23@gmail.com>
2015-09-30 12:31:40 -0700

    remove text from happy together

--- public/javascripts/vendor/tubular.js
+++ public/javascripts/vendor/tubular.js
@@ -1,5 +1,5 @@
 $('document').ready(function() {
-	var options = { videoId: 'e4Is32W-ppk', start: 3 };
+	var options = { videoId: 'P74YRczb84Q', start: 3 };
 	$('#wrapper').tubular(options);
 	// f-UGhWj1xww cool sepia hd
 	// 49SKbS7Xwf4 beautiful barn sepia
